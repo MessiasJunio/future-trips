7793aee1fa148705f968284406f2046c6e20bfaa
messias-junio <messiasjunio1998@gmail.com>
2021-03-29 16:43:27 -0400

    add Next SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://future-trips.messiasjunio.com.br',
+    site_name: 'Future Trips'
+  },
+  twitter: {
+    handle: '@Messias_Junio',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo'
+
 import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 import LinkWrapper from 'components/LinkWrapper'
 import { MapProps } from 'components/Map'
@@ -8,6 +10,26 @@ const Map = dynamic(() => import('components/Map'), { ssr: false })
 export default function HomeTemplate({ places }: MapProps) {
   return (
     <>
+      <NextSeo
+        title="Future Trips"
+        description="A simple project to show in a map the places that I want go and show more informations and photos when clicked."
+        canonical="https://future-trips.messiasjunio.com.br"
+        openGraph={{
+          url: 'https://future-trips.messiasjunio.com.br',
+          title: 'Future Trips',
+          description:
+            'A simple project to show in a map the places that I want go and show more informations and photos when clicked.',
+          images: [
+            {
+              url: 'https://future-trips.messiasjunio.com.br/img/cover.png',
+              width: 1280,
+              height: 720,
+              alt: 'My Trips'
+            }
+          ],
+          site_name: 'My Trips'
+        }}
+      />
       <LinkWrapper href="/about">
         <InfoOutline size={32} aria-label="About" />
       </LinkWrapper>
